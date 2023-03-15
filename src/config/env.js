dde8c68c23b5767be9370c0aa88b8e15342eecae
iagovtr <iagovitor_araujo@hotmail.com>
2023-03-14 21:46:12 -0300

    feat: add env file to export environment variables

---
+++ src/config/env.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const APP_PORT = process.env.APP_PORT || 4000;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PWD = process.env.DB_PWD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_DIALECT = process.env.DB_DIALECT;
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES = process.env.JWT_EXPIRES;
+
+module.exports = {
+  APP_PORT,
+  DB_NAME,
+  DB_USER,
+  DB_PWD,
+  DB_HOST,
+  DB_PORT,
+  DB_DIALECT,
+  JWT_SECRET,
+  JWT_EXPIRES,
+};
