fe48f4c17057888ea4c19123b57bcc9b45073d4d
iagovtr <iagovitor_araujo@hotmail.com>
2023-03-14 21:50:47 -0300

    feat: add authorization middleware function

---
+++ src/middlewares/authorization.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET, JWT_EXPIRES } = require('../config/env');
+
+const generateAuthToken = (payload) => {
+  const token = jwt.sign(payload, JWT_SECRET, {
+    expiresIn: JWT_EXPIRES,
+  });
+
+  return token;
+};
+
+const verifyAuthToken = (request, response, nextFunction) => {
+  const token = req.headers['Authorization'];
+
+  if (!token) {
+    return response.status(400).json({ error: 'no token provided' });
+  }
+
+  jwt.verify(token, JWT_SECRET, (error, decode) => {
+    if (error) {
+      return response
+        .status(500)
+        .json({ error: 'failed to authenticate token' });
+    }
+    request['tokenData'] = decode;
+    nextFunction();
+  });
+};
+
+module.exports = {
+  generateAuthToken,
+  verifyAuthToken,
+};
