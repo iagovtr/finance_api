49e42fb780fc1325d000ac83f3f18b00f5cbafcf
iagovtr <iagovitor_araujo@hotmail.com>
2023-03-24 20:39:19 -0300

    refactor: refactor expense route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const { authenticationRouter } = require('./authentication');
-const { invoicingRouter } = require('./invoicing');
 const { expenseRouter } = require('./expense');
 const { verifyAuthToken } = require('../middlewares/authorization');
 
 const router = express.Router();
 
 router.use('/auth', authenticationRouter);
-
-router.use('/invoicing', verifyAuthToken, invoicingRouter);
 
 router.use('/expense', verifyAuthToken, expenseRouter);
 
